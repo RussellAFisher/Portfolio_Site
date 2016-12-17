13de2b0fb4a9fe620a6bac69c23dcba0c249fb6e
Russell Fisher <russell_fisher@Russells-MacBook-Pro.local>
2016-12-17 13:06:30 -0700

    added images and changed to a side scroll for photography, photographyDetails is being disregarded for now but will stay in the file structure for possible rebuilds

--- public/app/app.js
+++ public/app/app.js
@@ -1,47 +1,18 @@
 var app = angular.module('portfolio', ['ui.router']);
 app.config(config);
 
-app.factory('Photography', function() {
-    return [{
-        id: 0,
-        name: 'test',
-        image: 'app/images/CarPower.png',
-    }, {
-        id: 1,
-        image: 'app/images/FireAlarmPi.png',
-    }, {
-        id: 2,
-        image: 'app/images/GPSHoming.png',
-    }, {
-        id: 3,
-        image: 'app/images/PowerRelay.png'
-    }, {
-        id: 4,
-        image: 'app/images/CarPower.png',
-    }, {
-        id: 5,
-        image: 'app/images/FireAlarmPi.png',
-    }, {
-        id: 6,
-        image: 'app/images/GPSHoming.png',
-    }, {
-        id: 7,
-        image: 'app/images/PowerRelay.png'
-    }];
-});
-
 function config($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/about');
 
     $stateProvider
         .state('about', {
             url: '/about',
-            controller: 'AboutController',
+            // controller: 'AboutController',
             templateUrl: 'app/views/about.html'
         })
         .state('projects', {
             url: '/projects',
-            controller: 'ProjectsController',
+            // controller: 'ProjectsController',
             templateUrl: 'app/views/projects.html'
         })
         .state('tinkering', {
@@ -56,7 +27,171 @@ function config($stateProvider, $urlRouterProvider) {
         })
         .state('photographyDetails', {
             url: '/photographyDetails/:photoID',
-            controller: 'PhotographyDetailsController',
+            // controller: 'PhotographyDetailsController',
             templateUrl: 'app/views/photographyDetails.html'
         });
 }
+
+app.factory('Photography', function() {
+    return [{
+        id: 0,
+        name: 'test',
+        image: 'app/images/photography/35mmBW01.jpg',
+    }, {
+        id: 1,
+        name: 'test',
+        image: 'app/images/photography/35mmBW02.jpg',
+    }, {
+        id: 2,
+        name: 'test',
+        image: 'app/images/photography/35mmBW03.jpg',
+    }, {
+        id: 3,
+        name: 'test',
+        image: 'app/images/photography/animal01.jpg',
+    }, {
+        id: 4,
+        name: 'test',
+        image: 'app/images/photography/animal02.jpg',
+    }, {
+        id: 5,
+        name: 'test',
+        image: 'app/images/photography/animal03.jpg',
+    }, {
+        id: 6,
+        name: 'test',
+        image: 'app/images/photography/closeUp01.jpg',
+    }, {
+        id: 7,
+        name: 'test',
+        image: 'app/images/photography/closeUp02.jpg',
+    }, {
+        id: 8,
+        name: 'test',
+        image: 'app/images/photography/closeUp03.jpg',
+    }, {
+        id: 9,
+        name: 'test',
+        image: 'app/images/photography/closeUp04.jpg',
+    }, {
+        id: 10,
+        name: 'test',
+        image: 'app/images/photography/closeUp05.jpg',
+    }, {
+        id: 11,
+        name: 'test',
+        image: 'app/images/photography/closeUp06.jpg',
+    }, {
+        id: 12,
+        name: 'test',
+        image: 'app/images/photography/industrial01.jpg',
+    }, {
+        id: 13,
+        name: 'test',
+        image: 'app/images/photography/industrial02.jpg',
+    }, {
+        id: 14,
+        name: 'test',
+        image: 'app/images/photography/industrial03.jpg',
+    }, {
+        id: 15,
+        name: 'test',
+        image: 'app/images/photography/industrial04.jpg',
+    }, {
+        id: 16,
+        name: 'test',
+        image: 'app/images/photography/landscape01.jpg',
+    }, {
+        id: 17,
+        name: 'test',
+        image: 'app/images/photography/landscape02.jpg',
+    }, {
+        id: 18,
+        name: 'test',
+        image: 'app/images/photography/landscape03.jpg',
+    }, {
+        id: 19,
+        name: 'test',
+        image: 'app/images/photography/landscape04.jpg',
+    }, {
+        id: 20,
+        name: 'test',
+        image: 'app/images/photography/landscape05.jpg',
+    }, {
+        id: 21,
+        name: 'test',
+        image: 'app/images/photography/landscape06.jpg',
+    }, {
+        id: 22,
+        name: 'test',
+        image: 'app/images/photography/landscape07.jpg',
+    }, {
+        id: 23,
+        name: 'test',
+        image: 'app/images/photography/landscape08.jpg',
+    }, {
+        id: 24,
+        name: 'test',
+        image: 'app/images/photography/nature01.jpg',
+    }, {
+        id: 25,
+        name: 'test',
+        image: 'app/images/photography/nature02.jpg',
+    }, {
+        id: 26,
+        name: 'test',
+        image: 'app/images/photography/nature03.jpg',
+    }, {
+        id: 27,
+        name: 'test',
+        image: 'app/images/photography/nature04.jpg',
+    }, {
+        id: 28,
+        name: 'test',
+        image: 'app/images/photography/nature05.jpg',
+    }, {
+        id: 29,
+        name: 'test',
+        image: 'app/images/photography/nature06.jpg',
+    }, {
+        id: 30,
+        name: 'test',
+        image: 'app/images/photography/nature07.jpg',
+    }, {
+        id: 31,
+        name: 'test',
+        image: 'app/images/photography/nature08.jpg',
+    }, {
+        id: 32,
+        name: 'test',
+        image: 'app/images/photography/nature09.jpg',
+    }, {
+        id: 33,
+        name: 'test',
+        image: 'app/images/photography/night01.jpg',
+    }, {
+        id: 34,
+        name: 'test',
+        image: 'app/images/photography/night02.jpg',
+    }, {
+        id: 35,
+        name: 'test',
+        image: 'app/images/photography/night03.jpg',
+    }, {
+        id: 36,
+        name: 'test',
+        image: 'app/images/photography/night04.jpg',
+    }, {
+        id: 37,
+        name: 'test',
+        image: 'app/images/photography/night05.jpg',
+    }, {
+        id: 38,
+        name: 'test',
+        image: 'app/images/photography/sunset01.jpg',
+    }, {
+        id: 39,
+        name: 'test',
+        image: 'app/images/photography/sunset02.jpg',
+    }];
+});
